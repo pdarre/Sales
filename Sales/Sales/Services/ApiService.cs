aaa9cbd1c888e24decd933b0605b9f765c787608
pablodarre <pdarre@gmail.com>
2018-09-20 11:45:06 -0300

    clase ApiService en Sales, clase Response generico para listas json en Common

---
+++ Sales/Sales/Services/ApiService.cs
@@ -0,0 +1,11 @@
+﻿namespace Sales.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class ApiService
+    {
+        public async Task<Response>
+    }
+}
